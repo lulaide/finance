8b4b1ff06b27d69b4c3d032a1b44dad2c75e5607
Bail <2915289604@qq.com>
2025-05-15 21:10:48 +0800

    改risk的bug

--- risk.py
+++ risk.py
@@ -12,7 +12,7 @@ symbols = [
     "sh601368", "sh603291", "sh603759",
 ]
 
-def calculate_var(symbol: str,
+def calculate_var(df:pd.DataFrame,
                   confidence_level: float = 0.95) -> pd.Series:
     """
     计算单只股票基于非重叠周度对数收益的历史模拟 VaR（百分比形式，正数表示损失）
@@ -22,12 +22,6 @@ def calculate_var(symbol: str,
     Returns:
       VaR 值（正数），表示该水平下一周最大可能损失比例
     """
-    # 获取日线行情
-    df = ak.stock_zh_a_daily(symbol)
-    # 转成 DatetimeIndex
-    df['date'] = pd.to_datetime(df['date'])
-    df = df.set_index('date').sort_index()
-
     # 计算对数收益
     df['log_ret'] = np.log(df['close']) - np.log(df['close'].shift(1))
     df = df.dropna(subset=['log_ret'])
@@ -45,13 +39,12 @@ def calculate_var(symbol: str,
     )
     return var_series
 
-def calculate_technical(symbol: str)->pd.Series:
+def calculate_technical(df: pd.DataFrame)->pd.Series:
     """计算技术风险
 symbols(str):股票代码
 返回值:技术风险(pd.Series)
 -----
 通过相对涨幅进行量化"""
-    df = ak.stock_zh_a_daily(symbol)
     # 计算MA20
     min_periods = 20
     df['MA20'] = df['close'].rolling(window=min_periods).mean()
@@ -67,22 +60,31 @@ def draw(name:str,stock:str,series: pd.Series):
 name(str):图表名称
 stock(str):股票代码
 series(pd.Series):数据"""
-    plt.title(f'Trend of {name}')
+    plt.title(f'Trend of {stock}_{name}')
     plt.xlabel('Time')
     plt.ylabel('Value')
     for i in series.index:
-        plt.plot(i,series.loc[i],'o')
+        plt.scatter(i,series.loc[i],marker='o',s=1)
     plt.savefig(f'photo/{stock}_{name}.png')
+    plt.close()
 
 def init():
     os.makedirs('photo', exist_ok=True)
 
+def get_dataframe(symbol:str)->pd.DataFrame:
+    # 获取日线行情
+    df = ak.stock_zh_a_daily(symbol)
+    # 转成 DatetimeIndex
+    df['date'] = pd.to_datetime(df['date'])
+    return df.set_index('date').sort_index()
+
 if __name__ == "__main__":
     init()
     for sym in symbols:
-        var = calculate_var(sym)
-        tech = calculate_technical(sym)
+        df = get_dataframe(sym)
+        var = calculate_var(df)
+        tech = calculate_technical(df)
         print(f"{sym} 一周市场风险 = {var.iloc[-1] * 100:.2f}%")
         print(f"{sym} 技术风险 = {tech.iloc[-1] * 100:.2f}%")
-        draw('VaR',sym,var)
+        ##draw('VaR',sym,var)
         draw('technical risk',sym,tech)
